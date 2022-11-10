5676cea6242e95e0881f93086769497f6c0dc7fc
Josué Makuta <makutajosue@gmail.com>
2022-11-10 15:33:12 +0100

    Feat : email checking design

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -1,4 +1,5 @@
 
+import { useCallback, useMemo } from "react"
 import { useState } from "react"
 import { inputStyles } from "../login/login"
 
@@ -7,6 +8,9 @@ const Signup = (props) => {
    const [matchedPWD, setMatchedPWD] = useState(false)
    const [repeatPassword, setRepeatPassword] = useState(false)
    const [showPassword, setShowPassword] = useState(false)
+   const [successMessage, setSuccessMessage] = useState('')
+   const [responseStatus, setResponseStatus] = useState(200)
+
 
    const handleUserName = (e) => {
       props.setUserName(e)
@@ -18,12 +22,18 @@ const Signup = (props) => {
       props.setUserPassword(e)
    }
    const passWordMatched = (passWord1, passWord2) => {
-      passWord1 == passWord2 ? setMatchedPWD(false) : setMatchedPWD(true);
+      if (passWord1 == passWord2) {
+         setMatchedPWD(false)
+         return true
+      } else {
+         setMatchedPWD(true);
+         return false
+      }
    }
 
 
    const insert_data = async () => {
-      await fetch("http://localhost:5000" + '/signup', {
+      await fetch(import.meta.env.VITE_USER_HOST_NAME + '/signup', {
          method: 'POST',
          mode: 'cors',
          headers: {
@@ -36,8 +46,13 @@ const Signup = (props) => {
                "passWord": props.userPassword
             }
          )
-      }).then((data) => console.log('Successfully inserted'))
-         .catch((err) => console.log('Failed to insert data !', err))
+      }).then(data => data.json())
+         .then((data) => {
+            console.log(data);
+            setSuccessMessage(data.message)
+            setResponseStatus(data.status)
+         })
+
    }
 
    return (
@@ -47,12 +62,24 @@ const Signup = (props) => {
             <p className=" font-bold text-2xl " >Create Account</p>
             <p>Hey, enter your details to get sign up to the Express chat app</p>
          </div>
-         <div className="flex flex-col text-start text-sm gap-4 w-full " >
+         <div className={responseStatus == 403 ? " text-red-600 text-4xl " : " text-green-600 text-4xl "} >
+            {
+               successMessage
+            }
+         </div>
+         <form
+            className="flex flex-col text-start text-sm gap-4 w-full "
+            onSubmit={(e) => {
+               e.preventDefault()
+               passWordMatched(props.userPassword, repeatPassword) ? insert_data() : null
+            }}
+         >
             <div className="flex gap-3 items-center justify-between ">
                <p>Name :</p>
                <input
                   type="text"
                   placeholder="Username"
+                  required={true}
                   className={inputStyles + "w-[50%]"}
                   onChange={(e) => { handleUserName(e.target.value) }}
                />
@@ -62,6 +89,7 @@ const Signup = (props) => {
                <input
                   type="email"
                   placeholder="makutajosue@gmail.com"
+                  required={true}
                   className={inputStyles + "w-[70%]"}
                   onChange={(e) => { handleUserEmail(e.target.value) }}
                />
@@ -70,12 +98,12 @@ const Signup = (props) => {
                <p>Password :</p>
                <input
                   type={showPassword ? 'text' : 'password'}
+                  required={true}
                   placeholder="Password"
                   className={inputStyles + "w-[60%]"}
                   onChange={(e) => { handleUserPassword(e.target.value) }}
-                  id="password"
                />
-               <button onClick={() => {
+               <button type="button" onClick={() => {
                   showPassword ? setShowPassword(false) : setShowPassword(true)
                }} >{showPassword ? 'Hide' : "Show"}</button>
             </div>
@@ -86,23 +114,18 @@ const Signup = (props) => {
                   placeholder="Password"
                   className={inputStyles + "w-[60%]"}
                   onChange={(e) => {
-                     passWordMatched(props.userPassword, repeatPassword)
                      setRepeatPassword(e.target.value);
-
                   }}
                />
             </div>
-         </div>
-         <div className=" flex flex-col gap-3 pt-5 mb-4 w-full" >
             <button
                className=" w-full h-10 rounded-lg bg-blue-400 "
-               onClick={() => {
-                  passWordMatched(props.userPassword, repeatPassword);
-                  insert_data()
-               }}
+               type="submit"
             >
                SIGNUP
             </button>
+         </form>
+         <div className=" flex flex-col gap-3 pt-5 mb-4 w-full" >
             {matchedPWD && <div
                className="font-['courrier']  text-red-500" >
                Passwords do not match.
