077007cdd1770ea71a7e07ac86e5fda4d4230eca
Josué Makuta <makutajosue@gmail.com>
2023-06-24 21:42:26 +0200

    Feat : save user into local storage and redirect after login

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,10 +1,18 @@
+import { Toaster } from "react-hot-toast";
 import Header from "./header/header";
 import LeftBar from "./leftbar/leftBar";
 import MainChat from "./mainChat/mainChat";
+import { useEffect } from "react";
+import { toast } from "react-hot-toast";
 
 const Chat = () => {
+  useEffect(() => {
+    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  }, []);
+
   return (
     <div className=" w-screen flex justify-center bg-gradient-to-tr from-main_color/30 to-white items-center h-full">
+      <Toaster />
       <div className="max-w-[1500px] bg-white max-h-[100vh] h-full w-full max-w-screen flex  flex-col  ">
         <Header />
         <main className=" flex h-full  justify-start md:pt-[60px] pt-[80px] ">
